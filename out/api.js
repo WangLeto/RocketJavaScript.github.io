272ee2b0d6691158b450448620fb18694de936ad
leto <1907984744@qq.com>
2017-08-23 03:09:26 +0800

    Add javaScript html docs.

---
+++ out/api.js
@@ -0,0 +1,50 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Hashes",
+        "Hashes",
+        "Hashes",
+        "Hashes",
+        "Hashes",
+        "Hashes",
+        "Hashes.RMD160",
+        "Hashes.SHA256",
+        "Hashes.SHA512",
+        "Popper.modifiers.Hashes.RMD160",
+        "Popper.modifiers.Hashes.SHA256",
+        "Popper.modifiers.Hashes.SHA512",
+        "Popper.modifiers.fecha",
+        "ReactClass",
+        "ReactDOMTextComponent",
+        "ReactMountReady",
+        "ReactMultiChild",
+        "ReactOwner",
+        "ReactPropTransferer",
+        "ReactReconcileTransaction",
+        "ReactServerRenderingTransaction",
+        "ReactShallowRenderer",
+        "ReactTestUtils",
+        "Transaction",
+        "api",
+        "state",
+        null
+    ],
+    "modules": [
+        "Api",
+        "Store"
+    ],
+    "allModules": [
+        {
+            "displayName": "Api",
+            "name": "Api",
+            "description": "This class contains some methods to connect backend."
+        },
+        {
+            "displayName": "Store",
+            "name": "Store",
+            "description": "This module contains classes for running a store."
+        }
+    ],
+    "elements": []
+} };
+});
